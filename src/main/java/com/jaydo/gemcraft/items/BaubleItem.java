7d58eb642a0994b1b5bc26044fd747acdaeb34ec
Justin Hineman <54921005+JayDo96@users.noreply.github.com>
2021-12-02 04:07:50 -0500

    Create BaubleItem.java

---
+++ src/main/java/com/jaydo/gemcraft/items/BaubleItem.java
@@ -0,0 +1,14 @@
+package com.jaydo.gemcraft.items;
+
+import com.jaydo.gemcraft.main.Main;
+import net.minecraft.world.item.Item;
+
+
+public class BaubleItem extends Item
+{
+
+	public MaterialItem(Properties properties) {
+		super(properties.tab(Main.GemcraftTabBauble));
+	}
+
+}
